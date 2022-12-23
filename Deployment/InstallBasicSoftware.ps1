f86a8eb08b4bdd008af8b4bdb02b29ddff50ccda
Kirill Novikov <GreezXII@protonmail.ch>
2022-12-23 10:29:33 +0300

    Fix the appearance of the UAC window twice

--- Deployment/InstallBasicSoftware.ps1
+++ Deployment/InstallBasicSoftware.ps1
@@ -1,20 +1,20 @@
-function Enable-Proxy {
-    $path = "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings"
-    Set-ItemProperty -Path $path -Name "ProxyEnable" -Value 1
-    Set-ItemProperty -Path $path -Name "ProxyServer" -Value "192.168.101.222:3128" 
-    Set-ItemProperty -Path $path -Name "ProxyOverride" -Value "192.168.*;<local>"
-}
-
-function Install-Choco {
-    Set-ExecutionPolicy Bypass -Scope Process -Force
+$installChoco = {
+    # Set up proxy
+    Write-Host "Proxy"
+    $path = '"Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings"'
+    Set-ItemProperty -Path $path -Name '"ProxyEnable"' -Value 1
+    Set-ItemProperty -Path $path -Name '"ProxyServer"' -Value '"192.168.101.222:3128"' 
+    Set-ItemProperty -Path $path -Name '"ProxyOverride"' -Value '"192.168.*;<local>"'
+    # Install choco
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('"https://community.chocolatey.org/install.ps1"'))
+
+    $installSoftware = {
+        Write-Host '"Run choco"'
+        choco install 7zip libreoffice-fresh adobereader thunderbird firefox googlechrome -y
+        Write-Host '"End!"'
+    }
+    Start-Process -FilePath PowerShell -ArgumentList """-ExecutionPolicy Bypass -Command & {$installSoftware}"""
 }
 
-function Install-Software {
-    choco install 7zip libreoffice-fresh adobereader thunderbird firefox googlechrome -y
-}
-
-Enable-Proxy
-Install-Choco
-Install-Software
+Start-Process -FilePath PowerShell -ArgumentList "-ExecutionPolicy Bypass -Command & {$installChoco}" -Verb RunAs -Wait
